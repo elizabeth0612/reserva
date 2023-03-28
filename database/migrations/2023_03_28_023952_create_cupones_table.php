798fb5e87b23c6086f8d1aa16899f0327aaaeef0
unknown <benitezvento@gmail.com>
2023-03-28 00:01:24 -0500

    Login, registre, roles, dashboard

---
+++ database/migrations/2023_03_28_023952_create_cupones_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cupones', function (Blueprint $table) {
+            $table->increments('id_cupon');
+            $table->double('descuento');
+            $table->boolean('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cupones');
+    }
+};
