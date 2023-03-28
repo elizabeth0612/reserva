522cde948ea30ad639effdfc3643e03edcf1a3dc
Elizabeth Marin <elizabethk0612@gmail.com>
2023-03-28 02:59:29 -0500

    AGREGANDO BOTON DESPLEGABLE RESERVAS

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -7,24 +7,18 @@
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0 d-flex text-wrap align-items-center" href=" {{ route('dashboard') }} ">
-            <img src="{{ asset('assets') }}/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
-            <span class="ms-2 font-weight-bold text-white">Material Dashboard 2 Laravel</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-person-circle" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+              </svg>
+            <span class="ms-2 font-weight-bold text-white">Admin</span>
         </a>
     </div>
-    <hr class="horizontal light mt-0 mb-2">
+    <hr class="horizontal light mt-0 mb-1">
     <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <li class="nav-item mt-3">
-                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Laravel examples</h6>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white {{ $activePage == 'user-profile' ? 'active bg-gradient-primary' : '' }} "
-                    href="{{ route('user-profile') }}">
-                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i style="font-size: 1.2rem;" class="fas fa-user-circle ps-2 pe-2 text-center"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">User Profile</span>
-                </a>
+                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Opciones</h6>
             </li>
             <li class="nav-item">
                 <a class="nav-link text-white {{ $activePage == 'user-management' ? ' active bg-gradient-primary' : '' }} "
@@ -32,9 +26,64 @@
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i style="font-size: 1rem;" class="fas fa-lg fa-list-ul ps-2 pe-2 text-center"></i>
                     </div>
-                    <span class="nav-link-text ms-1">User Management</span>
+                    <span class="nav-link-text ms-1">Inmuebles</span>
                 </a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link text-white {{ $activePage == 'user-profile' ? 'active bg-gradient-primary' : '' }} "
+                    href="{{ route('user-profile') }}">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i style="font-size: 1.2rem;" class="fas fa-user-circle  text-center"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">User Profile</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                      <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                            <a class="nav-link text-white "
+                            href="{{ route('user-profile') }}">
+                            <div class="text-white text-center me-1 d-flex align-items-center justify-content-center">
+                                <i style="font-size: 1.2rem;" class="fas fa-user-circle text-center"></i>
+                            </div>
+                            <span class="nav-link-text ms-1">Reservas</span>
+                        </a>
+                        </button>
+                      </h2>
+                      <div id="collapseOne" class="accordion-collapse collapse " aria-labelledby="headingOne" data-bs-parent="#headingOne">
+                        
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                                <a class="nav-link text-white" href="{{ route('dashboard') }}">
+                                <div class="text-white text-center me-1 d-flex align-items-center justify-content-center">
+                                    <i style="font-size: 1.2rem;" class="fas fa-user-circle text-center"></i>
+                                </div>
+                                <span class="nav-link-text ms-1">Reservas Pendientes</span>
+                            </a>
+                        </div>
+                            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                                <a class="nav-link text-white {{ $activePage == 'Reservas Realizadas' ? 'active bg-gradient-primary' : '' }} "
+                                href="{{ route('user-profile') }}">
+                                <div class="text-white text-center me-1 d-flex align-items-center justify-content-center">
+                                    <i style="font-size: 1.2rem;" class="fas fa-user-circle text-center"></i>
+                                </div>
+                                <span class="nav-link-text ms-1">Reservas Realizadas</span>
+                            </a>
+                        </div>
+                        </div>
+                            
+                        </div>
+
+                       
+                        </div>
+                    
+                    </div>
+
+                  </div>
+                
+            </li>
+          
             <li class="nav-item mt-3">
                 <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Pages</h6>
             </li>
