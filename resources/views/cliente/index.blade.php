8c647e459e5f9db657e5e79c0f97919da7f23df3
Elizabeth Marin <elizabethk0612@gmail.com>
2023-04-02 19:24:21 -0500

    MODIFICANDO LA VISTA DEL CLIENTE - PLUYINS

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -1,8 +1,10 @@
 <x-layout bodyClass="g-sidenav-show  bg-gray-200">
-    <x-navbars.sidebar activePage="profile"></x-navbars.sidebar>
-    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
-    <x-navbars.navs.auth titlePage="Dashboard"></x-navbars.navs.auth>
 
+    <x-navbars.sidebar activePage="user-management"></x-navbars.sidebar>
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+        <x-navbars.navs.auth titlePage="User Management"></x-navbars.navs.auth>
         <h1>LISTA DE CLIENTES</h1>
     </main>
+    <x-plugins></x-plugins>
+
 </x-layout>
