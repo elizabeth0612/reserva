c5b80ad5d4810a90ef6e66f1b08d8ff00fdfc80d
Elizabeth Marin <elizabethk0612@gmail.com>
2023-04-02 19:18:08 -0500

    EXTENDIENDO LA VISTA PARA CLIENTE

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -1,4 +1,8 @@
 <x-layout bodyClass="g-sidenav-show  bg-gray-200">
     <x-navbars.sidebar activePage="profile"></x-navbars.sidebar>
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+    <x-navbars.navs.auth titlePage="Dashboard"></x-navbars.navs.auth>
 
+        <h1>LISTA DE CLIENTES</h1>
+    </main>
 </x-layout>
