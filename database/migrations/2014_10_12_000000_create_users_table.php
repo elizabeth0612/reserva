5f5e23993981b0738e29c0058dd30148210c0eda
unknown <benitezvento@gmail.com>
2023-04-08 10:02:59 -0500

    Agregando nullable a los campos tabla cliente

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->nullable();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('phone')->nullable();
             $table->string('location')->nullable();
             $table->text('about')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->tinyInteger("role")->default(0);
             $table->timestamps();
